fbda70c056dd20c56004e7e0d66652866fe4d25d
pasantia <pasantia@dualbiz.net>
2019-03-28 14:17:03 -0400

    listo para producción

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Producto } from '../interfaces/producto.interface'
+import { Producto } from '../interfaces/producto.interface';
 
 
 @Injectable({
